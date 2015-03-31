ef7d3db61ea028dea17cf35937990216a961db78
Chenghua Hou <houchenghua@outlook.com>
2015-03-31 09:21:08 +0800

    Update index.php

--- SE/index.php
+++ SE/index.php
@@ -3,6 +3,8 @@ define('DS',DIRECTORY_SEPARATOR);
 define('ROOT',  dirname(__FILE__));
 define('PUBLIC_DIR',ROOT.DS.'public');
 define('APPLICATION',ROOT.DS.'application');
+define("WEB_ROOT",'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
+define("WEB_PUBLIC",WEB_ROOT.DS.'public');
 require_once(APPLICATION.DS.'bootstrap.php');
 Application::getInstance()->run(APPLICATION.DS.'config'.DS.'config.php');
 //echo "<br/><hr/>var_dump(config)<br/>";
